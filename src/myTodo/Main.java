913b6f17e9551035ddfe6bdc4711c3229a37874d
Yiga Gilbert <97894246+yigagilbert@users.noreply.github.com>
2022-11-22 13:29:29 +0300

    updates
    updates

--- src/myTodo/Main.java
+++ src/myTodo/Main.java
@@ -9,30 +9,29 @@ import task.Task;
 
 public class Main {
 	
-	public static ArrayList<String> tasks =new ArrayList<String>();
+	public static List<Task> tasks =new ArrayList<Task>();
 	public static Task task = new Task();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner input = new Scanner(System.in);
-		
+	
 		while(true) {
-			
-		System.out.println("\t\t.....ToDo Manager.....\t\t");
-        
-        System.out.println("What next \n 1. Enter task \n 2. View cur1rent tasks \n 3. Exit) ");
-        int step = input.nextInt();
-        switch (step) {
-            case 1:
-            	enterTask();
-                break;
-            case 2:
-            	viewTasks();
-                break;
-            case 3:
-                break;
-        }
-        
+			Scanner input = new Scanner(System.in);	
+			System.out.println("\t\t.....ToDo Manager.....\t\t");
+	        
+	        System.out.println("What next \n 1. Enter task \n 2. View cur1rent tasks \n 3. Exit) ");
+	        int step = input.nextInt();
+	        switch (step) {
+	            case 1:
+	            	enterTask();
+	                break;
+	            case 2:
+	            	viewTasks();
+	                break;
+	            case 3:
+	                break;
+	        }
+	        input.close();
 		}
 	}
 	
@@ -65,17 +64,18 @@ public class Main {
 		 Scanner input = new Scanner(System.in);
 		 System.out.println("Please Enter task title : ");
 //       String name = input.nextLine();
-       task.setName(input.next());
+       task.setName(input.nextLine());
        
        System.out.println("Description : ");
 //       String description = input.nextLine();
-       task.setDescription(input.next());
+       task.setDescription(input.nextLine());
        
        getChoice();
        
        System.out.println("Name \t Description \t Status");
        System.out.println(task.getName()+"\t"+task.getDescription()+"\t"+task.getStatus());
        
+       tasks.add(task);
       input.close();
 	 }
 	 
