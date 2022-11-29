d570a81ed20dd713622c51afd67cc6367ed0eced
Yiga Gilbert <vigidaiz15@gmail.com>
2022-11-29 12:14:35 +0300

    adding sql

---
+++ todo_app/src/main/java/org/pahappa/systems/todoapp/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.pahappa.systems.todoapp;
+
+/**
+ * @author google
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
